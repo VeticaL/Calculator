1bc2424655190982a1996d70714aa2c802004131
Vertical <ambintasso@gmail.com>
2022-12-17 19:01:46 +0530

    update js

--- main.js
+++ main.js
@@ -14,132 +14,35 @@ const btnAdd = document.getElementById("btnAdd");
 const btnSub = document.getElementById("btnSub");
 const btnDiv = document.getElementById("btnDiv");
 const btnEqual = document.getElementById("btnEqual");
+const displayText = document.getElementById("displaytext");
 
-let array1 = [];
-let array2 = [];
-let a;
-let b;
-let clicked = false;
-let operator;
+let string = "";
 
-btn1.onclick = () => {
-  if (clicked === false) {
-    array1.push("1");
-  } else if (clicked === true) {
-    array2.push("1");
+btn1.addEventListener("click", calculate);
+btn2.addEventListener("click", calculate);
+btn3.addEventListener("click", calculate);
+btn4.addEventListener("click", calculate);
+btn5.addEventListener("click", calculate);
+btn6.addEventListener("click", calculate);
+btn7.addEventListener("click", calculate);
+btn8.addEventListener("click", calculate);
+btn9.addEventListener("click", calculate);
+btn0.addEventListener("click", calculate);
+btnC.addEventListener("click", calculate);
+btnX.addEventListener("click", calculate);
+btnAdd.addEventListener("click", calculate);
+btnSub.addEventListener("click", calculate);
+btnDiv.addEventListener("click", calculate);
+btnEqual.addEventListener("click", calculate);
+
+function calculate() {
+  if (this.innerHTML == "=") {
+    string = eval(string);
+  } else if (this.innerHTML == "C") {
+    string = "";
+    displayText.value = string;
+  } else {
+    string = string + this.innerHTML;
+    displayText.value = string;
   }
-};
-
-btn2.onclick = () => {
-  if (clicked === false) {
-    array1.push("2");
-  } else if (clicked === true) {
-    array2.push("2");
-  }
-};
-
-btn3.onclick = () => {
-  if (clicked === false) {
-    array1.push("3");
-  } else if (clicked === true) {
-    array2.push("3");
-  }
-};
-
-btn4.onclick = () => {
-  if (clicked === false) {
-    array1.push("4");
-  } else if (clicked === true) {
-    array2.push("4");
-  }
-};
-
-btn5.onclick = () => {
-  if (clicked === false) {
-    array1.push("5");
-  } else if (clicked === true) {
-    array2.push("5");
-  }
-};
-
-btn6.onclick = () => {
-  if (clicked === false) {
-    array1.push("6");
-  } else if (clicked === true) {
-    array2.push("6");
-  }
-};
-
-btn7.onclick = () => {
-  if (clicked === false) {
-    array1.push("7");
-  } else if (clicked === true) {
-    array2.push("7");
-  }
-};
-
-btn8.onclick = () => {
-  if (clicked === false) {
-    array1.push("8");
-  } else if (clicked === true) {
-    array2.push("8");
-  }
-};
-
-btn9.onclick = () => {
-  if (clicked === false) {
-    array1.push("9");
-  } else if (clicked === true) {
-    array2.push("9");
-  }
-};
-
-btn0.onclick = () => {
-  if (clicked === false) {
-    array1.push("0");
-  } else if (clicked === true) {
-    array2.push("0");
-  }
-};
-
-btnC.onclick = () => {};
-
-btnX.onclick = () => {
-  clicked = true;
-  operator = "X";
-};
-
-btnAdd.onclick = () => {
-  operator = "Add";
-  if (clicked === false) {
-    // add array
-    for (let i = 0; i < array1.length; i += 1) {
-        a += array1[i];
-    }
-    clicked = true;
-    console.log(parseInt(a, 10));
-  } else if (clicked === true) {
-
-  }
-};
-
-btnSub.onclick = () => {
-  clicked = true;
-  operator = "Sub";
-};
-
-btnDiv.onclick = () => {
-  clicked = true;
-  operator = "Div";
-};
-
-btnEqual.onclick = () => {
-    switch (operator) {
-        case "Add":
-            
-            break;
-    
-        default:
-            break;
-    }
-};
+}
